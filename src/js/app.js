4b7b74dcdd99f1596ab423de3198b6f3e52f7e12
lena_pavlova <isuzu69@gmail.com>
2017-10-18 13:30:27 +0300

    start using angularjs for creating nav items and list of bookmarks

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+angular.module('bookmarksApp', [])
+	.controller('MainCtrl', function() {
+		var main = this;
+
+		main.categories = [
+      {"id": 0, "name": "Development"},
+      {"id": 1, "name": "Design"},
+      {"id": 2, "name": "Exercise"},
+      {"id": 3, "name": "Humor"}
+    ];
+
+    main.currentCategory = null;
+
+    main.setCurrentCategory = function(category) {
+    	main.currentCategory = category;
+    };
+
+    main.isCurrentCategory = function(category) {
+    	return (main.currentCategory === null && category === 'All')
+    	|| (main.currentCategory !== null && main.currentCategory.name === category.name);
+    };
+
+    main.bookmarks = [
+        {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
+        {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
+        {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
+        {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
+        {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
+        {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
+        {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
+        {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
+        {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+    ];
+		console.log(main);
+	});
