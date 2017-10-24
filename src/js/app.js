ed86a4458d2e56fd8f6074c1867b9804e64886bf
lena_pavlova <isuzu69@gmail.com>
2017-10-24 23:17:58 +0300

    make editing form work

--- src/js/app.js
+++ src/js/app.js
@@ -3,33 +3,80 @@ angular.module('bookmarksApp', [])
 		var main = this;
 
 		main.categories = [
-      {"id": 0, "name": "Development"},
-      {"id": 1, "name": "Design"},
-      {"id": 2, "name": "Exercise"},
-      {"id": 3, "name": "Humor"}
-    ];
+		  {"id": 0, "name": "Development"},
+		  {"id": 1, "name": "Design"},
+		  {"id": 2, "name": "Exercise"},
+		  {"id": 3, "name": "Humor"}
+		];
 
-    main.currentCategory = null;
+		main.currentCategory = null;
+		main.editedBookmark = null;
+		main.isEditing = false;
+		main.isCreating = false;
 
-    main.setCurrentCategory = function(category) {
-    	main.currentCategory = category;
-    };
+		main.setCurrentCategory = function(category) {
+			main.currentCategory = category;
+			main.cancelEditing();
+			main.cancelCreating();
+		};
 
-    main.isCurrentCategory = function(category) {
-    	return (main.currentCategory === null && category === 'All')
-    	|| (main.currentCategory !== null && main.currentCategory.name === category.name);
-    };
+		main.isCurrentCategory = function(category) {
+			return (main.currentCategory === null && category === 'All')
+			|| (main.currentCategory !== null && main.currentCategory.name === category.name);
+		};
 
-    main.bookmarks = [
-        {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
-        {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
-        {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
-        {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
-        {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
-        {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
-        {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
-        {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
-        {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
-    ];
+		main.bookmarks = [
+			{"id": 10, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
+			{"id": 11, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
+			{"id": 12, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
+			{"id": 13, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
+			{"id": 14, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
+			{"id": 15, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
+			{"id": 16, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
+			{"id": 17, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
+			{"id": 18, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+		];
 		console.log(main);
+
+		main.shouldShowCreating = function() {
+			return main.currentCategory && !main.isEditing;
+		}
+
+		main.shouldShowEditing = function() {
+			return main.isEditing && !main.isCreating;
+		}
+
+		main.startEditing = function(bookmark) {
+			main.isEditing = true;
+			main.isCreating = false;
+			main.editedBookmark = angular.copy(bookmark);
+		}
+
+		main.cancelEditing = function() {
+			main.isEditing = false;
+			main.editedBookmark = null;
+		}
+
+		main.startCreating = function() {
+			main.isCreating = true;
+			main.isEditing = false;
+		}
+
+		main.cancelCreating = function() {
+			main.isCreating = false;
+		}
+
+		main.updateBookmark = function(editedBookmark) {
+			var index = null;
+			for (var i = 0; i < main.bookmarks.length; i++) {
+				if (main.bookmarks[i].id === editedBookmark.id) {
+					index = i;
+					break;
+				}
+			}
+
+			main.bookmarks[i] = editedBookmark;
+			main.isEditing = false;
+			main.editedBookmark = null;
+		}
 	});
